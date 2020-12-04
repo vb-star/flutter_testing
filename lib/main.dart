ced766f5aa2461bfb3c6b014fbbe99c1a964483c
vaibhav <vaibhav.upadhyay@inventum.net>
2020-12-04 19:05:39 +0530

    Test -5

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Testing code magic'),
+      home: MyHomePage(title: 'Flutter Demo Home Pagez'),
     );
   }
 }
